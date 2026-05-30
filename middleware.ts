7ee993e09f71c2901758ef58c053ae24b0b1a253
mingyshellow-prog <mingyshellow@gmail.com>
2026-05-30 17:15:13 +0900

    fix: 미들웨어 matcher 범위 수정

--- middleware.ts
+++ middleware.ts
@@ -39,5 +39,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/dashboard/:path*', '/log/:path*', '/report/:path*', '/resume/:path*', '/auth'],
 }
